f1bf430dc5cc6b43b346578575cb0f373c71da8c
S FRANKLIN SURYA <franklinsurya4@gmail.com>
2026-03-07 09:31:08 +0530

    Connect frontend to production backend

--- taskmanagementsystem_Frontend/src/Api/axiosConfig.js
+++ taskmanagementsystem_Frontend/src/Api/axiosConfig.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:8080/api", // must match Spring Boot server
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
   },
 });
-
 export default api;
